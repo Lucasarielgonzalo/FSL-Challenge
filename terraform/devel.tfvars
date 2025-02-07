17b1b1f94830768aebde2b25456928130562ab17
Lucas Ariel Gonzalo <lgonzalo@Lucass-MacBook-Pro.local>
2025-02-07 13:28:21 -0300

    first test

---
+++ terraform/devel.tfvars
@@ -0,0 +1,3 @@
+bucket_name = "devel-fsl-challenge-lg"
+
+environment = "devel"
---
+++ terraform/main.tf
@@ -0,0 +1,31 @@
+resource "aws_s3_bucket" "fsl-chalenge-bucket" {
+  bucket = var.bucket_name
+
+  tags = {
+    name        = "My bucket"
+    environment = "Dev"
+  }
+}
+
+resource "aws_s3_bucket_website_configuration" "fsl-chalenge-website" {
+  bucket = aws_s3_bucket.fsl-chalenge-bucket.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+
+}
+
+
+resource "aws_s3_bucket_public_access_block" "fsl-chalenge-bucket-access" {
+  bucket = aws_s3_bucket.fsl-chalenge-bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = false
+  ignore_public_acls      = true
+  restrict_public_buckets = false
+}
